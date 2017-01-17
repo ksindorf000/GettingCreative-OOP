180b7d6cfc85947248824d9a3a12f18c6a7e771f
ksindorf000 <ksindorf000@gmail.com>
2017-01-17 16:50:01 -0500

    Working on validating currentPerson input

--- Program.cs
+++ Program.cs
@@ -21,18 +21,9 @@ namespace GettingCreative_OOP
             }
 
             Console.Clear();
-            Console.WriteLine("Here is your list: \n");
             DisplayPeople();
-
-            Console.WriteLine("\n Who would you like to see info on? ");
-            string personName = Console.ReadLine();
-            currentPerson = people.Find(x => x.name == personName);
-
-            Console.Clear();
-            Console.WriteLine(currentPerson.name + "\n");
-            currentPerson.DisplayHobbies();
-            Console.WriteLine("\n" + currentPerson.age +
-                "\n" + currentPerson.ReturnDOB());
+            GetCurrentPerson();
+            DisplayDetails();
 
         }
 
@@ -93,11 +84,60 @@ namespace GettingCreative_OOP
          ********************************************************************/
         static void DisplayPeople()
         {
+            Console.WriteLine("Here is your list of people: \n");
+
             foreach (var person in people)
             {
                 Console.Write(string.Join(", ", person) + "\n");
             }
         }
+
+        /********************************************************************
+        * GetCurrentPerson()                                               *
+        *   Method that gets and validates current Person based on name    *
+        ********************************************************************/
+        private static void GetCurrentPerson()
+        {
+            bool valid = true;
+            string personName;
+
+            do
+            {
+                Console.WriteLine("\n Who would you like to see info on? ");
+                personName = Console.ReadLine();
+
+                if (name.All(char.IsLetter))
+                {
+                    valid = true;
+                }
+                else
+                {
+                    valid = false;
+                    Console.WriteLine($"{name} is not valid. Try again. ");
+                }
+
+            } while (!valid);
+
+            currentPerson = people.Find(x => x.name == personName);
+        }
+
+        /********************************************************************
+         * DisplayDetails()                                                 *
+         *   Method that displays details for the requested Person          *
+         ********************************************************************/
+        private static void DisplayDetails()
+        {
+            Console.Clear();
+            Console.WriteLine(currentPerson.name + "\n");
+
+            currentPerson.DisplayHobbies();
+
+            Console.WriteLine("\n" +
+                currentPerson.age + "\n" +
+                currentPerson.ReturnDOB());
+        }
+
+
 
     }
 }
