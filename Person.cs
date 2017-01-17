bbca3811eb80af35dd70d301228b6a268e21ea59
ksindorf000 <ksindorf000@gmail.com>
2017-01-17 14:09:29 -0500

    Get names of People and add as instances of Person

---
+++ Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingCreative_OOP
+{
+    enum State
+    {
+        MortalEnemy,
+        Frienemy,
+        Acquaintance,
+        Friend,
+        CloseFriend
+    }
+
+    class Person
+    {
+
+        public string name;
+        private int friendPts;
+        public DateTime birthday;
+        private List<string> interests = new List<string>();
+
+        public Person(string _name)
+        {
+            name = _name;
+            friendPts = 0;
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
+
+        private void Birthday()
+        {
+
+
+        }
+
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,8 +8,67 @@ namespace GettingCreative_OOP
 {
     class Program
     {
+        static bool addPerson = true;
+        static string name;
+
         static void Main(string[] args)
         {
+            while (addPerson)
+            {
+                GetPersonList();
+            }
+
         }
+
+        /********************************************************************
+         * GetPersonList()                                                  *
+         *   Method that gets people and adds them as an instance of Person *
+         ********************************************************************/
+        static void GetPersonList()
+        {
+            string yN;
+            bool valid = true;
+
+            do
+            {
+                Console.WriteLine("Please enter the name of an acquaintance: ");
+                name = Console.ReadLine();
+
+                if (name.All(char.IsLetter))
+                {
+                    valid = true;
+                }
+                else
+                {
+                    valid = false;
+                    Console.WriteLine($"{name} is not valid. Try again. ");
+                }
+
+            } while (!valid);
+
+            Person person = new Person(name);
+
+            do
+            {
+                Console.WriteLine("\n Add another? (Y/N)");
+                yN = Console.ReadLine().ToLower();
+
+                if (yN != "y" && yN != "n")
+                {
+                    valid = false;
+                    Console.WriteLine($"{yN} is not a valid response. Try Again. ");
+                }
+                else
+                {
+                    valid = true;
+                    Console.WriteLine("\n");
+                }
+
+            } while (!valid);
+
+            addPerson = yN == "y" ? true : false;            
+        }
+
     }
 }
+
